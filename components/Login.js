9e025edd1353be21daa982c2b938559afacdde51
j-jh <54552839+j-jh@users.noreply.github.com>
2025-06-24 16:27:45 -0700

    feat (login): complete base functionality for static login form

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,6 @@
-// Register.js
-// Renders form component with username, password, pass confirmation, and input validation
-// Toggle show password, return to home screen, submit registration, clear buttons
+// Login.js
+// Renders form component with username, password, and input validation
+// Toggle show password, return to home screen, submit login, clear buttons
 
 // TODO: SUBMIT TO DATABASE
 // TODO: STYLING
@@ -9,13 +9,11 @@
 import { useState } from "react";
 import Link from "next/link";
 
-export default function Register() {
+export default function Login() {
     const [user, setUser] = useState("");
     const [pass, setPass] = useState("");
-    const [confirmPass, setConfirmPass] = useState("");
     // Stores error messages 
     const [errors, setErrors] = useState({
-        passMatch: '',
         passLength: '',
         userLength: '',
         // userTaken: ''
@@ -27,21 +25,15 @@ export default function Register() {
     /*
     Handles input validation upon form submission. Checks...
         - Username, password length
-        - Password, pass confirmation match
         - Sets errors to new error object
         - Confirms if valid from errors before submitting
     */
-    function onRegisterSubmit(e) {
+    function onLoginSubmit(e) {
         let validForm = true;
         e.preventDefault();
         const newError = {
-            passMatch: '',
             passLength: '',
             userLength: '',
-        }
-        if (pass !== confirmPass) {
-            newError.passMatch = "passwords do not match";
-            validForm = false;
         }
         if (user.length === 0 || user.length < 3 || user.length > 10) {
             newError.userLength = "username must be 3-10 characters long";
@@ -59,32 +51,29 @@ export default function Register() {
         console.log("user: ", user, "\npass: ", pass);
     }
 
-    // Clears user, pass, pass confirm, and error messages
+    // Clears user, pass, and error messages
     function handleClear() {
         setUser("");
         setPass("");
-        setConfirmPass("");
         setErrors({
-            passMatch: '',
             passLength: '',
             userLength: '',
         })
     }
     // Checks if any values in input fields to enable clear button
-    const enableClear = (user.length > 0) || (pass.length > 0) || (confirmPass.length > 0);
+    const enableClear = (user.length > 0) || (pass.length > 0);
 
     // Checks if all input fields are valid to enable submit button
-    const enableSubmit = (pass === confirmPass) && 
-    (pass.length >= 8 && pass.length <= 32) && 
+    const enableSubmit =  (pass.length >= 8 && pass.length <= 32) && 
     (user.length >= 3 && user.length <= 10);
 
-    // Checks if any values in pass fields to enable show/hide toggle
-    const enableShowHide = (pass.length > 0) || (confirmPass.length > 0);
+    // Checks if any values in pass field to enable show/hide toggle
+    const enableShowHide = (pass.length > 0);
 
     return (
         <div>
-            <h1>register</h1>
-            <form onSubmit={(e) => onRegisterSubmit(e)}>
+            <h1>login</h1>
+            <form onSubmit={(e) => onLoginSubmit(e)}>
                 <input type="text" placeholder="username" aria-label="Username" value={user}
                     onChange={(e) => setUser(e.target.value)} />
                 <br />
@@ -92,23 +81,16 @@ export default function Register() {
 
                 <input type={showPass ? "text" : "password"} placeholder="password" value={pass}
                     onChange={(e) => setPass(e.target.value)} />
+                <button type="button" disabled={!enableShowHide} 
+                    onClick={() => setShowPass(!showPass)}>{!showPass ? 'show' : 'hide'}</button>
                 <br />
                 {errors.passLength && <p style={{ color: 'red' }}>{errors.passLength}</p>}
 
-                <input type={showPass ? "text" : "password"} placeholder="confirm password" value={confirmPass}
-                    onChange={(e) => setConfirmPass(e.target.value)} />
-                <br />
-                {errors.passMatch && <p style={{ color: 'red' }}>{errors.passMatch}</p>}
-
-                <button type="button" disabled={!enableShowHide} 
-                onClick={() => setShowPass(!showPass)}>{!showPass ? 'show' : 'hide'}</button>
-                <br />
-
                 {/* disable buttons until valid*/}
-                <button type="submit" disabled={!enableSubmit}>meet my cat!</button>
+                <button type="submit" disabled={!enableSubmit}>take me to my cat!</button>
                 <button type="button" disabled={!enableClear} onClick={handleClear}>clear</button>
             </form>
             <Link href="/"><button type = "button">return...</button></Link>
         </div>
-    ) 
+    )
 }
