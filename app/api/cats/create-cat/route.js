ad9a09e7fb54a89f0d6a526cb7a81282f63e28ff
j-jh <54552839+j-jh@users.noreply.github.com>
2025-07-26 21:53:38 -0700

    fix (POST API): update API erorr and success returns

--- app/api/cats/create-cat/route.js
+++ app/api/cats/create-cat/route.js
@@ -1,6 +1,7 @@
 // route: /api/cats/create-cat
 // Creates a cat for first time uers
 import pool from "@/services/database";
+import { apiError, apiSuccess } from "@/services/response";
 
 // TODO: for this method call, pass in the user ID directly instead of finding it from request
 // temp keep as http req query for testing 
@@ -25,17 +26,33 @@ export async function POST(req) {
         const { searchParams } = new URL(req.url, "http://localhost"); // base url
         console.log("line 26");
         const userID = searchParams.get('userid');
+        if (!userID) {
+            return apiError(
+                "Missing parameter: ID",
+                400
+            )
+        }
         const catName = searchParams.get('catname')
+        if (!catName) {
+            return apiError(
+                "Missing parameter: catName",
+                400
+            )
+        }
         console.log(userID, catName);
         const newCat = await pool.query(`INSERT INTO cats 
             (userid, name, hunger, money, affection, energy, exp) 
             VALUES ($1, $2, 0, 0, 0, 0, 0) RETURNING *`, [userID, catName]);
-        return Response.json(newCat.rows);
 
+        return apiSuccess(
+            "Successfully submitted cat data",
+            newCat.rows,
+            200
+        );
     } catch (error) {
-        return Response.json({
-            msg: "Failed to create cat",
-            msg: error.message
-        });
+        return apiError(
+            "Failed to process POST request",
+            500
+        )
     }
 }
