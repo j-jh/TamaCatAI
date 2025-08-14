5b500395218404f1f10576ef9a7209060d816ff0
j-jh <greenjello106@gmail.com>
2025-08-13 22:18:04 -0700

    feat (verify user API): add API to verify user's JWT

---
+++ app/api/users/verify-user/route.js
@@ -0,0 +1,42 @@
+/* 
+    route.js
+    -----
+    Contains API endpoint to verify a user's JSON web token
+*/
+
+import { verifyUser } from "@/services/authentication";
+import { apiError, apiSuccess } from "@/services/errorResponses";
+/*
+    GET API
+    /api/users/verify-user
+
+    Checks whether JSON Web Token from HTTP request header matches user's
+
+    Function Parameter:
+    - req: HTTP request object containing method, URL, query params, body 
+
+    Expected HTTP Header:
+    - Authorization: Bearer <JWT>
+
+    Behaviors:
+    - Extracts JWT from request's authorization header 
+    - Verifies extracted token with verifyUser()
+    - Returns decoded token info about user on success
+    - Error if token missing/invalid/expired
+*/
+export async function GET(req) {
+  try {
+    const user = verifyUser(req);
+    return apiSuccess(
+        "Successful JWT match", 
+        user, 
+        200
+    );
+  } catch (error) {
+    return apiError(
+        error.message, 
+        401
+    );
+  }
+}
+
