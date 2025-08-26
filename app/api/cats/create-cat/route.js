ca6d80dba0c08de3b4ea779e3b1844686ed34848
j-jh <greenjello106@gmail.com>
2025-08-25 20:16:26 -0700

    feat (Create Cat API): add JWT verificiation checkpoint for API

--- app/api/cats/create-cat/route.js
+++ app/api/cats/create-cat/route.js
@@ -1,48 +1,72 @@
-// route: /api/cats/create-cat
-// Creates a cat for first time uers
+/*
+    route.js: Create Cat API
+    =====
+    Creates a cat for first time uers
+    
+    Database table reference:
+    Cats
+    ---------------------------------------------------------------------------
+    id  | userid | name | hunger | money | affection | energy | exp | bday
+    ---------------------------------------------------------------------------
+    int | int    | char | int    | int   | int       | int    | int | timestamp
+*/
 import pool from "@/services/database";
 import { apiError, apiSuccess } from "@/services/errorResponses";
-
-// TODO: for this method call, pass in the user ID directly instead of finding it from request
-// temp keep as http req query for testing 
+import { verifyUser } from "@/services/authentication";
 
 /*
-    POST API endpoint
-    Parameter of HTTP request object with user ID as query 
-    Queries to add cat row to db with matching user ID
-    Returns
-    Error
+    POST API
+    /api/cats/create-cat
 
-    need to input based on what user enters as name, and user ID
-    for ref:
-        Table: cats 
-    ---------- 
-    id  | userid | name | hunger | money | affection | energy | exp | bday
-    int | int    | char | int    | int   | int       | int    | int | timestamp
+    Creates a new cat for the authenticated user
+
+    Function Parameter:
+    - req: HTTP request object containing method, URL, query params, body 
+    
+    Expected JSON Request Body:
+        {
+            "catName": "string"
+        }
+
+    Expected HTTP Header:
+    - Authorization: Bearer <JWT>
+
+    Behaviors:
+    - Validates JWT in Authorization Header
+    - Extracts id from decoded JWT payload
+    - Inserts cat with specified name into database
+    - Returns a success message with the cat info on success
+    - Returns an error if:
+        - Missing, invalid JSON body
+        - Missing user ID
+        - Backend/DB error
+        - Invalid, missing, or expired JWT
 */
 export async function POST(req) {
     try {
-        console.log("createing new cat...")
-        const { searchParams } = new URL(req.url, "http://localhost"); // base url
-        console.log("line 26");
-        const userID = searchParams.get('userid');
-        if (!userID) {
-            return apiError(
-                "Missing parameter: ID",
-                400
-            )
-        }
-        const catName = searchParams.get('catname')
+        // Verify JWT from auth head, throws err if invalid
+        const user = verifyUser(req);
+
+        // Extract id from payload
+        const { id } = user;
+        const body = await req.json();
+        const { catName } = body;
+        // if (!id) {
+        //     return apiError(
+        //         "Missing parameter: ID",
+        //         400
+        //     )
+        // }
         if (!catName) {
             return apiError(
                 "Missing parameter: catName",
                 400
             )
         }
-        console.log(userID, catName);
+        console.log(id, catName);
         const newCat = await pool.query(`INSERT INTO cats 
             (userid, name, hunger, money, affection, energy, exp) 
-            VALUES ($1, $2, 0, 0, 0, 0, 0) RETURNING *`, [userID, catName]);
+            VALUES ($1, $2, 0, 0, 0, 0, 0) RETURNING *`, [id, catName]);
 
         return apiSuccess(
             "Successfully submitted cat data",
