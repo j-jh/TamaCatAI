054e57d147e4577b67c402df3a4e345cacb3fa0f
j-jh <greenjello106@gmail.com>
2025-09-02 23:25:21 -0700

    feat (Onboarding): connect Onboard component to create cat API

--- components/Onboarding.js
+++ components/Onboarding.js
@@ -1,13 +1,37 @@
-// Onboard.js
-// Welcome screen for first time users to set cat name
+/*
+    Onboarding.js
+    ----- 
+    Welcome page for first time users to set cat name
 
-// TODO: Connect to db
-// TODO: Styling 
+    Props/State:
+    - cat: Global cat state for cat data
+    = name: string of cat name
+    - hasName: boolean for name existence 
+    - errors: object with error messages for validation, API errors
+    - awaitAPI: boolean to store API call await status
+    - focus: 
+
+    Handlers and Behaviors:
+    - handleSubmitName()
+        Ensures cat name length is valid
+        Updates cat state with new name, clears errors 
+    - handleClear()
+        Clears name, errors state
+    - createCat()
+        Fetches post request to /api/cats/create-cat with JWT, catName
+        Returns cat data on success, null on failure or API error, updates error state
+    - handleBegin()
+        Checks hasName state to confirm if name set
+        Calls createCat() 
+        Routes user to dashboard on success
+
+    Additional notes:
+    -
+*/
 
 "use client";
 import { useState } from "react";
 import { useCat } from "@/context/CatContext";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Onboarding() {
@@ -15,20 +39,19 @@ export default function Onboarding() {
     const { cat, setCat } = useCat();
     const [name, setName] = useState("");
     const [focus, setFocus] = useState(false);
-    // Boolean to track if name set
     const [hasName, setHasName] = useState(false);
-
+    const [awaitAPI, setAwaitAPI] = useState(false);
     // Error object for storing messages
     const [errors, setErrors] = useState({
         nameLen: '',
         nameSet: '',
+        apiError: '',
     })
 
     // Checks name length validation before setting name
     function handleSubmitName() {
-        // Check name length, return error if req not met
         if (name.length <= 2 || name.length > 8) {
-            setErrors(prevErrors => ({...prevErrors, nameLen: 'name must be 3-8 characters'}));
+            setErrors(prevErrors => ({ ...prevErrors, nameLen: 'name must be 3-8 characters' }));
             return;
         }
         setCat({
@@ -38,14 +61,63 @@ export default function Onboarding() {
         setErrors({
             nameLen: '',
             nameSet: '',
+            apiError: '',
         });
+
+
     }
-    
-    // Checks name set before routing to dashboard
-    function handleBegin() {
+    // Fetches cat api, passes user ID as JWT payload and catName as JSON
+    async function createCat() {
+        try {
+            const response = await fetch('/api/cats/create-cat', {
+                method: "POST",
+                body: JSON.stringify(
+                    {
+                        catName: name,
+                    }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+                }
+            })
+
+            const data = await response.json();
+            console.log(data);
+            if (!response.ok) {
+                setErrors(prev => ({
+                    ...prev,
+                    apiError: data.message,
+                }))
+                return null;
+
+            }
+            return data;
+
+        } catch (error) {
+            console.log("api err");
+            console.log(errors.apiError);
+            setErrors(prev => (
+                {
+                    ...prev,
+                    apiError: error.message
+                }));
+            return null;
+        }
+    }
+
+    // Checks name set, calls createCat(), routes to dashboard if successful
+    async function handleBegin() {
         // Return error if name not set
         if (!hasName) {
-            setErrors(prevErrors => ({...prevErrors, nameSet: 'name must be first set'}))
+            setErrors(prevErrors => ({ ...prevErrors, nameSet: 'name must be first set' }))
+            return;
+        }
+        setAwaitAPI(true);
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        const newCatSuccess = await createCat();
+        setAwaitAPI(false);
+        if (!newCatSuccess) {
+            // Set error
             return;
         }
         // Route to dashboard
@@ -55,27 +127,32 @@ export default function Onboarding() {
     // Disables clear button when input null
     const enableSubmit = name.length > 0;
 
+    // Clears errors, name field
     function handleClear() {
         setName("");
         setErrors({
             nameLen: '',
             nameSet: '',
+            apiError: '',
         });
     }
-    
+
     return (
         <div>
             <h1>test</h1>
             <h1>{cat.name}</h1>
-            <input value={name} 
+            <input value={name}
                 onFocus={() => setFocus(!focus)}
                 onChange={(e) => setName(e.target.value)}
-                placeholder="gimme a good name"/>
+                placeholder="gimme a good name" />
             <button type="button" onClick={handleSubmitName}>set name</button>
             <button type="button" onClick={handleClear} disabled={!enableSubmit}>clear</button>
-            {errors.nameLen ? <p style={{ color: 'red' }}>{errors.nameLen}</p> : <><br/> <br/></>}
-            {errors.nameSet ? <p style={{ color: 'red' }}>{errors.nameSet}</p> : <><br/></>}
-            <button type="button" onClick={handleBegin}>let's begin!</button>
+            {errors.nameLen ? <p style={{ color: 'red' }}>{errors.nameLen}</p> : <><br /> <br /></>}
+            {errors.nameSet ? <p style={{ color: 'red' }}>{errors.nameSet}</p> : <><br /></>}
+            {errors.apiError ? <p style={{ color: 'red' }}>{errors.nameSet}</p> : <><br /></>}
+            <button type="button" onClick={handleBegin} disabled={awaitAPI}>
+                {!awaitAPI ? "let's begin!" : "making cat..."}
+            </button>
         </div>
     )
 }
