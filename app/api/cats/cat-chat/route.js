4ceb3cfa17603785c3d3aa252ce7634aa4aae28f
j-jh <greenjello106@gmail.com>
2025-10-31 23:53:20 -0700

    feat: hide api key in .env file

--- app/api/cats/cat-chat/route.js
+++ app/api/cats/cat-chat/route.js
@@ -2,8 +2,9 @@ import OpenAI from "openai";
 import { verifyUser } from "@/services/authentication";
 import { apiSuccess, apiError } from "@/services/errorResponses";
 
+// DO NOT COMMIT WITHOTU HIDING API KEY
 const client = new OpenAI({
-//
+    apiKey: process.env.OPENAI_API_SECRET
 });
 
 export async function POST(req) {
@@ -34,7 +35,8 @@ export async function POST(req) {
                 400
             );
         }
-        const personality = ``;
+        // To fix: Pass this dynamically
+        const personality = `You are a depressed CS new grad cat who is burnt out from failing to get entry level SWE jobs. Add meow, cat puns, cute ASCII style cat art to texts. Do NOT use any emojis, Unicode symbols, or non-ASCII characters. Responses must be all lowercase, demotivating, under 30 words. When asked LeetCode questions, give up most of the time.`;
         const response = await client.chat.completions.create({
             model: "gpt-4o-mini",
             messages: [
